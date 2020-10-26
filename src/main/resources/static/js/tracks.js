bb9a4486b52b77cb75d107d65d4c5b401f8d48c9
Simon Peppitt <simonworkpersonal@gmail.com>
2020-10-26 14:55:41 +0000

    add track.js, update tracks.html

--- src/main/resources/static/js/tracks.js
+++ src/main/resources/static/js/tracks.js
@@ -30,10 +30,19 @@ fetch('http://localhost:8082/tracks/read')
     let row = thead.insertRow();
 
     for (let keys of data){
-        let th = document.createElement("th");
-        let text = document.createTextNode(keys);
-        th.appendChild(text);
-        row.appendChild(th);
+        console.log(keys);
+        if (keys == 'id'){
+            
+        } else if (keys == 'playlist') {
+
+        } else {
+            let th = document.createElement("th");
+            let text = document.createTextNode(keys);
+            th.appendChild(text);
+            row.appendChild(th);
+        }
+
+
     }
     let editHead = document.createElement("th");
     let editButtonTitle = document.createTextNode("Edit");
@@ -44,28 +53,32 @@ fetch('http://localhost:8082/tracks/read')
 
 function createTableBody(table,dataData){
     for (let dataRecord of dataData){
+
         let row = table.insertRow();
         for (value in dataRecord){
+            if (value == 'id'){
+
+            } else if (value == 'playlist'){
+
+            } else {
           let cell = row.insertCell();
           let text = document.createTextNode(dataRecord[value]);
           if (typeof dataRecord[value] === 'object'){
             for (object in dataRecord[value]){
-              let taskText = document.createTextNode(dataRecord[value][object].name);
-              cell.appendChild(taskText);
-
-              if (dataRecord[value].indexOf(dataRecord[value][object]) < dataRecord[value].length-1) {
-                let comma = document.createTextNode(", ");
-                cell.appendChild(comma);
-              }
+                if (object == 'name'){
+                    let albumText = document.createTextNode(dataRecord.album.name);
+                  cell.appendChild(albumText);
+                }
             }
           } else{
             cell.appendChild(text);
           }
         }
+        }
         let editCell = row.insertCell();
         let editButton = document.createElement("a");
         editButton.className="btn btn-primary";
-        editButton.href="userRecord.html?id="+dataRecord.id;
+        // editButton.href="userRecord.html?id="+dataRecord.id;
         editButton.innerHTML="Edit";
         editCell.appendChild(editButton);
 
