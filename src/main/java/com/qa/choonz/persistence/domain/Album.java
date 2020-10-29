95841c2bb0c5d603221058df548233d62117d7eb
Md Joni <mjoni@qa.com>
2020-10-29 12:08:07 +0000

    Update latest change in this branch

--- src/main/java/com/qa/choonz/persistence/domain/Album.java
+++ src/main/java/com/qa/choonz/persistence/domain/Album.java
@@ -31,7 +31,7 @@ public class Album {
     @Column(unique = true)
     private String name;
 
-    @OneToMany(mappedBy = "album", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "album", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference(value = "album")
     private List<Track> tracks = new ArrayList<>();
 
--- src/main/java/com/qa/choonz/persistence/domain/Track.java
+++ src/main/java/com/qa/choonz/persistence/domain/Track.java
@@ -2,12 +2,7 @@ package com.qa.choonz.persistence.domain;
 
 import java.util.Objects;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -26,14 +21,12 @@ public class Track {
     @Column(unique = true)
     private String name;
 
-    @ManyToOne
+    @ManyToOne()
     @JsonBackReference(value = "album")
     private Album album;
 
-    @ManyToOne
-//    @JsonIgnoreProperties("playlist")
+    @ManyToOne()
     @JsonBackReference(value = "playlist")
-    @JsonIgnoreProperties(value = { "playlist" })
     private Playlist playlist;
 
     // in seconds
