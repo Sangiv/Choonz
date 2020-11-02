9c9f44c3df7c7eb53683ab21a45980acd193eb54
Md Joni <mjoni@qa.com>
2020-11-02 00:36:34 +0000

    user has all the home functionality in index page

--- src/main/resources/static/js/userAuth.js
+++ src/main/resources/static/js/userAuth.js
@@ -1,8 +1,22 @@
 let userAuth =document.cookie;
-console.log(userAuth)
-if(userAuth != null || userAuth != undefined){
-    console.log("Session started")
-}else{
+// let userPass = document.cookie;
+ console.log(userAuth)
+var userBtn = document.getElementById("userLoggedInBtn");
+var guestBtn = document.getElementById("guestLoggedInBtn");
+
+//if user logged in then change hidden button button
+if(userAuth != ""){
+    console.log("Inner Session started")
+    if(window.getComputedStyle(guestBtn).display === "none"){
+        guestBtn.style.display = "none";
+        userBtn.style.display = "block";
+    }
+}
+else{
     console.log("Session timed out")
-    window.location.href= "index.html"
+    if(window.getComputedStyle(userBtn).display==="none"){
+        guestBtn.style.display = "block";
+        userBtn.style.display = "none";
+    }
+   
 }
