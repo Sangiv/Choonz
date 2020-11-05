37f9feb8c4fb97e30edcd3c235c34b03521ed130
Simon Peppitt <simonworkpersonal@gmail.com>
2020-11-05 12:00:48 +0000

    tidy album.js and adjust albumview.js

--- src/main/resources/static/js/album.js
+++ src/main/resources/static/js/album.js
@@ -6,13 +6,9 @@ fetch('http://localhost:8082/albums/read')
           response.status);
         return;
       }
-
       console.log('Fetch Success')
       response.json().then(function(dataData) {
-        console.log(dataData);
-
         cardData(dataData);
-
       });
     }
   )
@@ -20,45 +16,41 @@ fetch('http://localhost:8082/albums/read')
     console.log('Fetch Error :-S', err);
   });
 
-  function createCard(id, image, title, description, buttonText, buttonLink, artistLink){
-    //updates cloneCard with new information
-    let cards = document.querySelector("div.showcards");
-    let cloneCard = document.querySelector("div.card").cloneNode(true);
-    cloneCard.id = ("card" + id);
-    cloneCard.querySelector("img").src=(image);
-    cloneCard.querySelector("#imagelink").href=(buttonLink);
-    cloneCard.querySelector("#titlelink").innerHTML = (title);
-    cloneCard.querySelector("#titlelink").href = (buttonLink);
-    cloneCard.querySelector("#textlink").innerHTML = (description);
-    cloneCard.querySelector("#textlink").href = (artistLink);
-    cloneCard.querySelector("#button").innerHTML = (buttonText);
-    cloneCard.querySelector("#button").href = (buttonLink);
-    cards.appendChild(cloneCard);
-  }
-
-  function cardData(dataData){
-
-    for (let dataRecord of dataData){
-        singleIterationCheck = 0;
-        for (value in dataRecord){
-
-            if (typeof dataRecord[value] === 'object'){
-              if (singleIterationCheck != 0){
-
-              } else {
-                let id = dataRecord.id;
-                let image = dataRecord.cover;
-                let title = dataRecord.name;
-                let description = dataRecord.artist.name;
-                let buttonText = "View";
-                let buttonLink = "albumview.html?id="+id;
-                let artistLink = "artistalbums.html?id=" + dataRecord.artist.id;
-                createCard(id, image, title, description, buttonText, buttonLink, artistLink);
-                singleIterationCheck++;
-              }
+//loops through and extracts data from json, assigns it to variables
+function cardData(dataData){
+  for (let dataRecord of dataData){
+      singleIterationCheck = 0;
+      for (value in dataRecord){
+          if (typeof dataRecord[value] === 'object'){
+            if (singleIterationCheck != 0){
+            } else {
+              let id = dataRecord.id;
+              let image = dataRecord.cover;
+              let title = dataRecord.name;
+              let description = dataRecord.artist.name;
+              let buttonText = "View";
+              let buttonLink = "albumview.html?id="+id;
+              let artistLink = "artistalbums.html?id=" + dataRecord.artist.id;
+              createCard(id, image, title, description, buttonText, buttonLink, artistLink);
+              singleIterationCheck++;
             }
-
-        }
-    }
+          }
+      }
   }
+}
 
+//clones a hidden html card, attaches data, and appends to html
+function createCard(id, image, title, description, buttonText, buttonLink, artistLink){
+  let cards = document.querySelector("div.showcards");
+  let cloneCard = document.querySelector("div.card").cloneNode(true);
+  cloneCard.id = ("card" + id);
+  cloneCard.querySelector("img").src=(image);
+  cloneCard.querySelector("#imagelink").href=(buttonLink);
+  cloneCard.querySelector("#titlelink").innerHTML = (title);
+  cloneCard.querySelector("#titlelink").href = (buttonLink);
+  cloneCard.querySelector("#textlink").innerHTML = (description);
+  cloneCard.querySelector("#textlink").href = (artistLink);
+  cloneCard.querySelector("#button").innerHTML = (buttonText);
+  cloneCard.querySelector("#button").href = (buttonLink);
+  cards.appendChild(cloneCard);
+}
